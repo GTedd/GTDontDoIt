a0f7c466c43b967972099c58b4bdff82f14f080a
GTedd <1976416309@qq.com>
2025-11-04 11:09:16 +0800

    核心升级1.21.8->1.21.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     kotlin("jvm") version "2.1.21"
-    id("com.gradleup.shadow") version "8.3.0"
+    id("com.gradleup.shadow") version "9.2.2"
     id("xyz.jpenilla.run-paper") version "2.3.1"
+    // id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
 }
 
 group = "net.astrorbits"
-version = "1.21.8-1.0.3"
+version = "1.21.10-1.0.3"
 
 repositories {
     mavenCentral()
@@ -15,8 +16,8 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
-    compileOnly(files("libs/paper-1.21.8.jar"))
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    compileOnly(files("libs/paper-1.21.10.jar"))
 }
 
 tasks {
@@ -24,7 +25,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21.8")
+        minecraftVersion("1.21.10")
     }
 }
 
